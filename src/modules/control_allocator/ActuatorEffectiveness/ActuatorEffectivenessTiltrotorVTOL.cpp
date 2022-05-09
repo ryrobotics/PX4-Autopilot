8a7542ad377eb35dbacede1fdd1e75741f620725
Silvan Fuhrer <silvan@auterion.com>
2022-05-09 15:29:38 +0200

    Control Allocation: add option to disable yaw by differential thrust
    This replaces the propeller_torque_disabled flag to disable yaw by differential thrust
    for tiltrotor and tailsitter VTOLs, as propeller_torque_disabled is not enough to set
    effectiveness of an acutator in the yaw axis to 0 in case it's tilted.
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/control_allocator/ActuatorEffectiveness/ActuatorEffectivenessTiltrotorVTOL.cpp
+++ src/modules/control_allocator/ActuatorEffectiveness/ActuatorEffectivenessTiltrotorVTOL.cpp
@@ -60,7 +60,8 @@ ActuatorEffectivenessTiltrotorVTOL::getEffectivenessMatrix(Configuration &config
 
 	// MC motors
 	configuration.selected_matrix = 0;
-	_mc_rotors.enablePropellerTorque(!_tilts.hasYawControl());
+	_mc_rotors.enableYawByDifferentialThrust(!_tilts.hasYawControl());
+	_mc_rotors.enableThreeDimensionalThrust(false);
 
 	// Update matrix with tilts in vertical position when update is triggered by a manual
 	// configuration (parameter) change. This is to make sure the normalization
